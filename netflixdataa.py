b720b935cf3b55230c9c4ae01c45ab25f69e3055
Devendra Kumar Gehlot <devendragehlot0125@gmail.com>
2025-06-25 11:33:00 +0530

    Update netflixdataa.py

--- netflixdataa.py
+++ netflixdataa.py
@@ -30,7 +30,7 @@ ax[0][0].set_title('Number of shows produced by top countries ', fontsize=10)
 ax[0][0].legend()
 ax[0][0].set_xlabel('Number of production')
 ax[0][0].set_ylabel('Country')
-print(f"Contries's contribution to netflix: {country.index[0]} has the most number of movies and shows on Netflix. This might be because it's an English-speaking country and has a big film industry.")
+print(f"Contries's contribution to netflix: {countries_and_production.index[0]} has the most number of movies and shows on Netflix. This might be because it's an English-speaking country and has a big film industry.")
 
 
 ax[0][1].bar(Types_of_production.index, Types_of_production.values, color=['#f28e2b','#4e79a7'], label=['TV shows','Movies'])
@@ -38,7 +38,7 @@ ax[0][1].set_title('Number of content published', fontsize=10)
 ax[0][1].set_xlabel('Types')
 ax[0][1].set_ylabel('Quantity of published type')
 ax[0][1].legend()
-print(f"Types of content: {show.index[1]}s are more than {show.index[0]}s on Netflix. Maybe people prefer watching movies more, or they are easier to produce.")
+print(f"Types of content: {Types_of_production.index[1]}s are more than {Types_of_production.index[0]}s on Netflix. Maybe people prefer watching movies more, or they are easier to produce.")
 
 
 ax[1][0].plot(Release_years.index, Release_years.values, marker='o')
@@ -46,13 +46,13 @@ ax[1][0].set_title('Content published since 2008 to 2020', fontsize=10)
 ax[1][0].set_xlabel('Years')
 ax[1][0].set_ylabel('Content published')
 ax[1][0].grid(axis='x', color='lightgrey')
-max_year = year[year == year.values.max()].index[0]
+max_year = Release_years[Release_years == Release_years.values.max()].index[0]
 print(f"Published content per year: The highest number of shows and movies were added in {max_year}. After that, it started decreasing. Maybe because of COVID or other reasons.")
 
 
-ax[1][1].pie(High_ratings.values, labels=rating.index , autopct='%1.1f%%',colors=['#ff9999','#66b3ff','#99ff99','#ffcc99','#c2c2f0'])
+ax[1][1].pie(High_ratings.values, labels=High_ratings.index , autopct='%1.1f%%',colors=['#ff9999','#66b3ff','#99ff99','#ffcc99','#c2c2f0'])
 ax[1][1].set_title('Top 5 ratings distribution', fontsize=10)
-top_rating = rating.idxmax()
+top_rating = High_ratings.idxmax()
 print(f"Rating: The most common rating in the top 5 is '{top_rating}'. That means Netflix has a lot of content for older audiences.")
 
 
